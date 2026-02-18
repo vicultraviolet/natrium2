dd6b02f7caf0c2af36db4c96c311bbd9fd671f7d
goofyahhvic <95176727+goofyahhvic@users.noreply.github.com>
2026-02-18 12:00:45 -0300

    added glfw initialization

--- src/Core/Context.cpp
+++ src/Core/Context.cpp
@@ -3,38 +3,62 @@
 
 #include "Natrium2/Core/Logger.hpp"
 
-#if defined(NA2_PLATFORM_WINDOWS)
-#include <Windows.h>
+#ifdef NA2_PLATFORM_WINDOWS
+	#include <Windows.h>
 #endif // NA_PLATFORM_WINDOWS
+
+#ifdef NA2_USE_GLFW
+	#include <GLFW/glfw3.h>
+#endif // NA2_USE_GLFW
 
 namespace Na2
 {
 	static std::filesystem::path getExecPath(void)
 	{
-	#if defined(NA_PLATFORM_LINUX)
+#if defined(NA_PLATFORM_LINUX)
 		return std::filesystem::canonical("/proc/self/exe");
-	#elif defined(NA_PLATFORM_WINDOWS)
+#elif defined(NA_PLATFORM_WINDOWS)
 		char exec_path_buffer[MAX_PATH];
 		GetModuleFileNameA(nullptr, exec_path_buffer, MAX_PATH);
 		return exec_path_buffer;
-	#else
+#else
 		return "";
-	#endif // NA_PLATFORM
+#endif // NA_PLATFORM
 	}
 
 	Context::Context(const ContextInitInfo& info)
-	: m_ExecPath(getExecPath()),
-	m_ExecDir(m_ExecPath.parent_path()),
-	m_ExecName(m_ExecPath.filename()),
-	m_Version("Pre-Alpha 2")
+		: m_ExecPath(getExecPath()),
+		m_ExecDir(m_ExecPath.parent_path()),
+		m_ExecName(m_ExecPath.filename()),
+		m_Version("Pre-Alpha 2")
 	{
 		g_Logger.print_header();
 		g_Logger.printf(Info, "Initializing Natrium version {}", m_Version);
+
+	#ifdef NA2_USE_GLFW
+		glfwSetErrorCallback([](int error, const char* description)
+		{
+			if (error == 65539)
+				return;
+
+			g_Logger.printf(Error, "GLFW Error#{}: {}", error, description);
+			throw std::runtime_error(NA2_FORMAT("GLFW Error #{}", error));
+		});
+
+		int result = glfwInit();
+		NA2_ASSERT(result, "Failed to initialize glfw!");
+
+		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+	#endif // NA2_USE_GLFW
 	}
 
 	void Context::destroy(void)
 	{
 		g_Logger.print(Info, "Shutting down Natrium, Goodbye!");
+
+	#ifdef NA2_USE_GLFW
+		glfwTerminate();
+	#endif // NA2_USE_GLFW
 
 		if (Context::s_Context == this)
 			s_Context = nullptr;
